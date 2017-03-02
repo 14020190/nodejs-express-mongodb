fb842ce716e0ab5bc1fc22a35a6582a5170f81c2
Huy Hoang <nghuyhoang96.isp1415@gmail.com>
2017-03-03 00:47:15 +0700

    update tech_week1.html

--- web/index.php
+++ web/index.php
@@ -46,14 +46,14 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		<!---End-wrap--->
 	<div class="content">
 
-		<?php
-		switch ($_GET["page"]){
-			case "techweek1": include("tech_week1.html");
-				break;
-			default: include_once("index.php");
-				break;
-		}
-		?>
+<!--		--><?php
+//		switch ($_GET["page"]){
+//			case "techweek1": include("tech_week1.html");
+//				break;
+//			default: include_once("index.php");
+//				break;
+//		}
+//		?>
 
 		<div class="grid1" id="me">
 			<a href="#"><img src="images/face.gif" title="name" /></a>
@@ -89,7 +89,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				<div class="grid3-left">
 					<h3>Chuyên đề công nghệ</h3>
 					<ul>
-						<li><a href="index.php?page=techweek1"><img src="images/Places-folder-black-icon.png" titl="Icon-Name" /></a></li>
+						<li><a href="index.php?page=techweek1" target="_blank"><img src="images/Places-folder-black-icon.png" titl="TechWeek1" /></a></li>
 						<li><a href="#"><img src="images/Places-folder-black-icon.png" titl="Icon-Name" /></a></li>
 						<li><a href="#"><img src="images/Places-folder-black-icon.png" titl="Icon-Name" /></a></li>
 						<li><a href="#"><img src="images/Places-folder-black-icon.png" titl="Icon-Name" /></a></li>
